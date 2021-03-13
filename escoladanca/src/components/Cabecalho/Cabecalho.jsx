87081d8267e0a3a599ade2ff0e9750ddebddb1c4
CaioKeidi <caiokeidi@gmail.com>
2021-03-13 20:34:41 -0300

    feat: add some style to logo

--- escoladanca/src/components/Cabecalho/Cabecalho.jsx
+++ escoladanca/src/components/Cabecalho/Cabecalho.jsx
@@ -5,6 +5,11 @@ import './Cabecalho.css'
 const Cabecalho = () => {
     return (
         <div className='cabecalho-container'>
+            <div className='logo-area'>
+                <div className='aura-logo'>AURA</div>
+                <div className='escola-logo'>estúdio de dança</div>
+            </div>
+            <div className='provisorio'></div>
         </div>
     );
 }
