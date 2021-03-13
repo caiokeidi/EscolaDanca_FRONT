a9d0567a5a17a8a87ab47a18599385c7a15ac54a
CaioKeidi <caiokeidi@gmail.com>
2021-03-13 14:18:19 -0300

    first_commit

---
+++ escoladanca/src/components/Cabecalho/Cabecalho.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Cabecalho.css'
+
+
+const Cabecalho = () => {
+    return (
+        <div className='cabecalho-container'>
+        </div>
+    );
+}
+
+export default Cabecalho;
