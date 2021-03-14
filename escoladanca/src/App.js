c7ee9379d5e817a908428ca7dba6da528b46d532
CaioKeidi <caiokeidi@gmail.com>
2021-03-13 23:23:26 -0300

    feat: Home COmponent

--- escoladanca/src/App.js
+++ escoladanca/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Cabecalho from './components/Cabecalho/Cabecalho';
 import Footer from './components/Footer/Footer';
+import Home from './paginas/Home/Home';
 
 function App() {
   return (
     <>
       <Cabecalho/>
+      <Home/>
       <Footer/>
     </>
   );
---
+++ escoladanca/src/paginas/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css'
+
+const Home = () => {
+    return(
+        <div className='page-area'>
+            <div className='container'>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Home;
