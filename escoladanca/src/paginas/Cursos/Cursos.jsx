4703c95eeaabd2401071aeffb065d0b022b29c5a
CaioKeidi <caiokeidi@gmail.com>
2021-03-20 16:23:39 -0300

    feat: add animation to cards on Curso Page

--- escoladanca/src/paginas/Cursos/Cursos.jsx
+++ escoladanca/src/paginas/Cursos/Cursos.jsx
@@ -8,19 +8,20 @@ const Cursos = (props) => {
 
     return(
         <div className='page-area'>
-            <div className='container'>
+            <div className='container anim1'>
                 <div className='page-title-area'>
                     <h1>Cursos</h1>
                 </div>
                 <div className='page-content-area'>
                     <div className='card-curso'>
                         <div className='card-over-area'>
-                            <h2 className='card-title'>Título</h2>
+                            <h2 className='card-title'>Zouk</h2>
                         </div>
                         
                         <img className='card-img' src={img.default} alt='imagem dança de salão'/>
                     </div>
-                </div>
+                </div>    
+                
                 
             </div>
         </div>
