d416208f95b36914f26f8c589c3e919eed2b08b3
CaioKeidi <caiokeidi@gmail.com>
2021-03-22 16:29:52 -0300

    some changes on contato.jsx

--- escoladanca/src/paginas/Contato/Contato.jsx
+++ escoladanca/src/paginas/Contato/Contato.jsx
@@ -5,8 +5,19 @@ import './Contato.css'
 const Contato = () => {
     return(
         <div className='page-area'>
-            <div className='container'>
-                <h1>Contato</h1>
+            <div className='container anim1'>
+                <div className='page-title-area'>
+                    <h1>Contato</h1>
+                </div>
+                <div className="page-text-area">
+                <p className="page-text-p1">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id
+                    doloremque quas aut minima enim officiis illum accusantium
+                    repellendus! Est quod necessitatibus quibusdam veritatis architecto
+                    nisi earum optio incidunt quaerat. Quo.
+                </p>
+                </div>
+                
             </div>
         </div>
     )
