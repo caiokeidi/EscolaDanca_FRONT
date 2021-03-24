2d83a6a38b9265eddc10eab70b31ae209f6deb33
CaioKeidi <caiokeidi@gmail.com>
2021-03-23 23:29:32 -0300

    feat: Contato Page Style add

--- escoladanca/src/paginas/Contato/Contato.jsx
+++ escoladanca/src/paginas/Contato/Contato.jsx
@@ -19,21 +19,24 @@ const Contato = () => {
 
                 <div className='page-form-area'>
                     <form action='' className='page-form'>
-                        <div className='page-form-row'>
+                        <div className='page-form-block'>
                             <label to='nome'>Nome: </label>
                             <input type='text' name='nome' className='page-form-field'></input>
                         </div>
-                        <div className='page-form-row'>
+                        <div className='page-form-block'>
                             <label to='email'>Email: </label>
                             <input type='email' name='email' className='page-form-field'></input>
                         </div>
-                        <div className='page-form-row'>
+                        <div className='page-form-block'>
                             <label to='assunto'>Assunto: </label>
                             <input type='text' name='assunto' className='page-form-field'></input>
                         </div>
-                        <div className='page-form-row'>
+                        <div className='page-form-block'>
                             <label to='mensagem'>Mensagem: </label>
                             <textarea name='mensagem' className='page-form-textarea'></textarea>
+                        </div>
+                        <div className='page-form-block'>
+                            <input type='submit' placeholder='Enviar' className='page-form-submit'/>
                         </div>
                     </form>
 
